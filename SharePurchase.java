24187e868238b8d59321d261be19f1b98b9ffaba
Cho Yee Sin <yeesincho7@gmail.com>
2023-03-14 15:32:32 +0800

    Update SharePurchase.java

--- SharePurchase.java
+++ SharePurchase.java
@@ -4,9 +4,7 @@ package examples;
  * SharePurchase.java An immutable class that represents the purchase of one
  share of stock.
  *
- * @author Frank M. Carrano
- * @version 2.0
- */
+*/
 public class SharePurchase {
 
   private double cost;
